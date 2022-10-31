fba31d63d8d969afa65fb21b34cb6e692843dbbe
Sourav Saha <60583363+SouravSaha18@users.noreply.github.com>
2022-10-31 11:31:44 +0600

    Add files via upload

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="">Sourav Saha 19CSE029</a>. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
